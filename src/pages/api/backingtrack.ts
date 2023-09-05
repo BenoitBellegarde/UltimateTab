0367800419b48a1bb4a7b4234af6d9748da4e3f4
BenoitBellegarde <benoit.bellegarde@orange.fr>
2023-09-05 09:07:26 +0200

    chore: formatting

--- src/pages/api/backingtrack.ts
+++ src/pages/api/backingtrack.ts
@@ -6,30 +6,31 @@ export default async function handlerTab(
 ) {
   const keywords = req.query.q as string
   if (keywords) {
-    const apiKey = process.env.YOUTUBE_API_KEY;
-    const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=${encodeURIComponent(keywords)}&type=video&part=id&maxResults=1`;
+    const apiKey = process.env.YOUTUBE_API_KEY
+    const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=${encodeURIComponent(
+      keywords,
+    )}&type=video&part=id&maxResults=1`
 
     try {
-        const response = await fetch(apiUrl);
+      const response = await fetch(apiUrl)
 
-        if (response.ok) {
-            const data = await response.json();
-            const videoId = data.items[0]?.id?.videoId;
+      if (response.ok) {
+        const data = await response.json()
+        const videoId = data.items[0]?.id?.videoId
 
-            if (videoId) {
-                res.status(200).json(`https://www.youtube.com/watch?v=${videoId}`)
-                res.status(200).json(`https://www.youtube.com/watch?v=adV8-_hgL4g&ab`)
-
-            } else {
-                res.status(500).json({ error: 'error fetching youtube backing track' })
-            }
+        if (videoId) {
+          res.status(200).json(`https://www.youtube.com/watch?v=${videoId}`)
         } else {
-            res.status(500).json({ error: 'error fetching youtube backing track' })
+          res
+            .status(500)
+            .json({ error: 'error fetching youtube backing track' })
         }
-    } catch (error) {
+      } else {
         res.status(500).json({ error: 'error fetching youtube backing track' })
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'error fetching youtube backing track' })
     }
-
   } else {
     res.status(500).json({ error: 'error fetching youtube backing track' })
   }
